23238b4218cc26b286003907517b721f43507245
saarp <saarp@liveperson.com>
2020-08-09 16:07:49 +0300

    added bla bla

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, Saar!" << std::endl;
     std::cout << "Hello, David!" << std::endl;
+    std::cout << "Hello, David!" << std::endl;
+    std::cout << "Hello, David!" << std::endl;
     return 0;
 }
