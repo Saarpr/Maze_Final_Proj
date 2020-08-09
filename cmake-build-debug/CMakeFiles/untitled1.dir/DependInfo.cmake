4c3f4333a9835a5b2833fcba6b1275735a9b5165
saarp <saarp@liveperson.com>
2020-08-09 16:03:33 +0300

    test1

--- cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/saarp/CLionProjects/untitled1/main.cpp" "/Users/saarp/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/main.cpp.o"
+  "/Users/saarp/CLionProjects/Maze_Final_Proj/main.cpp" "/Users/saarp/CLionProjects/Maze_Final_Proj/cmake-build-debug/CMakeFiles/untitled1.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- main.cpp
+++ main.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Saar!" << std::endl;
+    std::cout << "Hello, David!" << std::endl;
     return 0;
 }
